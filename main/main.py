88631ff29862d8fdd4c555d1ce0456bcb93e49e0
LilRowdy <76605688+LilRowdy@users.noreply.github.com>
2021-07-25 18:26:48 -0600

    wepStat Gen added
    names can be randomized now based on a text document.
    Wep stats have been added to a list and can now be generated.
    Wep stats have an associating tier to them in the text document

--- main/main.py
+++ main/main.py
@@ -18,19 +18,60 @@ def charecterClassGen():
 
 def wepName():
 
-    with open("python3Projects\TF2randomWeaponGen\main\wepNameAdj.txt", "r") as f:
+    with open("TF2randomWeaponGenerator\main\wepNameAdj.txt", "r") as f:
         contentsAdj = f.readlines()
         f.close
 
-    with open("python3Projects\TF2randomWeaponGen\main\wepNameNoun.txt", "r") as f:
+    with open("TF2randomWeaponGenerator\main\wepNameNoun.txt", "r") as f:
         contentsNoun = f.readlines()
         f.close
-    
+
     randAdj = contentsAdj[random.randint(0, len(contentsAdj))]
     randNoun = contentsNoun[random.randint(0, len(contentsNoun))]
 
-    randWepName = str(randAdj) + str(randNoun)
+    randAdj = str(randAdj.replace("\n", ""))
+    randNoun = str(randNoun.replace("\n", ""))
 
+    randWepName = randAdj + " " + randNoun
+    
     return randWepName
 
-print(wepName())
+def wepStats():
+
+    itemSelection = ["w", "u", "m"]
+    damageType = ["e", "b", "p", "m", "u"]
+    
+    wepType = [itemSelection[random.randint(0, len(itemSelection)-1)], damageType[random.randint(0, len(damageType)-1)]]
+
+    if wepType[0] == "m":
+        wepType[1] = "m"
+    elif wepType[0] == "u":
+        wepType[1] = "u"
+
+    with open("TF2randomWeaponGenerator\main\statList", "r") as f:
+        statsContent = f.readlines()
+        
+        randStat = statsContent[random.randint(0, len(statsContent)-1)]
+
+        f.close
+    
+    randStat = randStat.strip("\n")
+
+    randStat = randStat.split(" ")
+    
+    statOverType = randStat[-1]
+    statOverType = list(statOverType)
+    print(statOverType)
+    
+    statTier = statOverType[0]
+    statWepType = statOverType[1]
+    if len(statOverType) == 2:
+        statTier = statOverType[0]
+        statWepType = statOverType[1]
+    elif len(statOverType) == 3:
+        statTier = statOverType[:2]
+        statWepType = statOverType[2]
+
+    print(statTier)
+    print(statWepType)
+wepStats()
